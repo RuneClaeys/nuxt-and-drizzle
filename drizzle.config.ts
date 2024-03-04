8702955a37186a3eee194708999f5deb071d0866
Rune Claeys <rune.claeys@ago.jobs>
2024-03-04 11:38:09 +0100

    init drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./database/schema.ts",
+  out: "./database/migrations",
+  driver: "better-sqlite",
+  dbCredentials: {
+    url: "./database/sqlite.db",
+  },
+});
