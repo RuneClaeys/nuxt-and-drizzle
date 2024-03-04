6ed994ca251ed934c07087032c2bc05513b612b5
Rune Claeys <rune.claeys@ago.jobs>
2024-03-04 11:44:20 +0100

    Remove blogs table and related files, add books and chapters tables

--- database/schema.ts
+++ database/schema.ts
@@ -1,8 +1,22 @@
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { relations } from "drizzle-orm";
+import { pgTable, serial, text } from "drizzle-orm/pg-core";
 
-export const blogs = sqliteTable("blogs", {
-  id: integer("id").primaryKey({ autoIncrement: true }),
+export const books = pgTable("books", {
+  id: serial("id").primaryKey(),
   name: text("name").notNull(),
-  createdAt: integer("created_at", { mode: "timestamp" }).notNull(),
-  updatedAt: integer("updated_at", { mode: "timestamp" }).notNull(),
+  author: text("author"),
 });
+
+export const bookRelations = relations(books, ({ many }) => ({
+  chapters: many(chapters),
+}));
+
+export const chapters = pgTable("chapters", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  bookId: serial("book_id"),
+});
+
+export const chapterRelations = relations(chapters, ({ one }) => ({
+  book: one(books),
+}));
